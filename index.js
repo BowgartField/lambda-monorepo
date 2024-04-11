974531d6503bd8212bef8ee4df84a0c7fc7a701d
Jean-Benoît <jeanbenoit@realt.co>
2024-04-11 11:52:44 +0300

    feat: add support to another filter file

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const fs = require('fs');
 const YAML = require('yaml');
 const shell = require('shelljs');
 
-const deployAll = ({ fnc, yml, zipParams, alias, layer }) => {
+const deployAll = ({ functions, yml, zipParams, alias, layer }) => {
   let success = true;
-  const { code } = shell.exec(`sh ./deploy.sh "${fnc}" "${yml[key][0].split('*')[0]}" "${zipParams}" "${alias}" "${layer}"`);
-  if (code) {
-    console.error(`Deployment of ${key} failed!`);
-    success = false;
+  for (const [key, value] of Object.entries(functions)) {
+    if (value === 'true') {
+      const { code } = shell.exec(`sh ./deploy.sh "${key}" "${yml[key][0].split('*')[0]}" "${zipParams}" "${alias}" "${layer}"`);
+      if (code) {
+        console.error(`Deployment of ${key} failed!`);
+        success = false;
+      }
+    }
   }
   return success;
 };
@@ -19,7 +23,8 @@ const run = async () => {
     const zipParams = core.getInput('zip-params');
     const alias = core.getInput('alias-name');
     const layer = core.getInput('layer-name');
-    const file = fs.readFileSync('./.github/filters.yml', 'utf8');
+    const filterFilePath = core.getInput('filter-file-path') ?? './.github/filters.yml';
+    const file = fs.readFileSync(filterFilePath, 'utf8');
     const yml = YAML.parse(file);
 
     const success = deployAll({ lambdaFunctions, yml, zipParams, alias, layer });
